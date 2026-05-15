70f0b5e49172d247f062cebd219e054676880c31
Sharmila S <sharmiseenivasan00@gmail.com>
2026-05-15 12:25:36 +0530

    Time: 4 ms (100.00%) | Memory: 45.9 MB (71.97%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution{
+    public boolean isPalindrome(int x){
+        if(x<0 ||(x%10==0&&x!=0)){
+            return false;
+        }
+        int reversedHalf=0;
+        while(x>reversedHalf){
+            int pop=x%10;
+            reversedHalf=reversedHalf*10+pop;
+            x/=10;
+        }
+        return(x==reversedHalf || x==reversedHalf/10);
+    }
+}
+
