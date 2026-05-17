3bb68d249a8b67f32ac0a38cc6305cfab236a28c
Sharmila S <sharmiseenivasan00@gmail.com>
2026-05-17 13:02:43 +0530

    Time: 2 ms (82.42%) | Memory: 53.1 MB (21.97%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,16 @@
+class Solution{
+    public boolean canReach(int[] arr, int start){
+        return dfs(arr,start,new boolean[arr.length]);
+    }
+    private boolean dfs(int[] arr,int i,boolean[] visited){
+        if(i<0 || i>=arr.length || visited[i]){
+            return false;
+        }
+        if (arr[i]==0){
+            return true;
+        }
+        visited[i]=true;
+        return dfs(arr,i+arr[i], visited) || 
+               dfs(arr,i-arr[i], visited);
+    }
+}
